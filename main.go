30f17643e42d773e28ab70b3704af5a755fbfd43
Faithfulness Alamu <vaguemail369@gmail.com>
2020-11-24 22:09:14 +0100

    Add main function
     - Validate code arg
     - Print result from corpus

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	flag "github.com/spf13/pflag"
+)
+
+var (
+	errNoCode      = errors.New("no code given as argument")
+	errInvalidCode = errors.New("invalid code")
+	errOutOfRange  = errors.New("code is out of range")
+)
+
+var verbose = flag.BoolP("verbose", "v", false, "display all available info about the status code")
+
+func main() {
+	if len(os.Args) <= 1 {
+		log.Fatal(errNoCode)
+	}
+
+	code, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal(errInvalidCode)
+	}
+
+	if !isInRange(code) {
+		log.Fatal(errOutOfRange)
+	}
+
+	if *verbose {
+		printVerbose(code)
+	} else {
+		printReason(code)
+	}
+}
+
+func printVerbose(code int) {
+	httpCode, err := getDetails(code)
+	if err != nil {
+		log.Fatal(explainCorpusError(err))
+	}
+	fmt.Printf(`%d\n%s\n%s\n%s\n`, httpCode.code, httpCode.reasonPhrase, httpCode.description, httpCode.moreinfoLink)
+}
+
+func printReason(code int) {
+	reasonPhrase, err := getReasonPhrase(code)
+	if err != nil {
+		log.Fatal(explainCorpusError(err))
+	}
+	fmt.Println(reasonPhrase)
+}
+
+func explainCorpusError(err error) error {
+	if err == errCodeNotFound {
+		return errors.New("this is a non-standard response, possibly custom to the server's software.")
+	}
+	return err
+}
+
+//isInRange returns true if code is in the range 100-599
+func isInRange(code int) bool {
+	return 100 <= code && code <= 599
+}
